7aaac17b587482328bc4ae8a13e69be1e3cabd6c
Irene Teinemaa <irene@localhost.localdomain>
2013-06-15 20:33:04 +0300

    design

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,8 @@
 class PostsController < ApplicationController
+  before_filter :signed_in_user
+  before_filter :correct_user_for_create ,  only: [:create]
+  before_filter :correct_user_for_delete ,  only: [:delete]
+
   def create
     @room = Room.find(params[:room_id])
     param_hash = params[:post]
@@ -13,4 +17,21 @@ class PostsController < ApplicationController
     @post.destroy
     redirect_to room_path(@room)
   end
+
+  private
+
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+  
+ def correct_user_for_create
+      @room = Room.find(params[:room_id])
+      redirect_to(root_path) unless @room.members.include?(current_user)
+    end
+
+  def correct_user_for_delete
+      @room = Room.find(params[:room_id])
+      @post = @room.posts.find(params[:id])
+      redirect_to(root_path) unless @post.username == current_user.name || @room.owner == current_user
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   attr_accessible :name, :password, :password_confirmation
   has_secure_password
 
-  has_many :owned_rooms, :class_name => "Room", :foreign_key => "owner_id"
+  has_many :owned_rooms, :class_name => "Room", :foreign_key => "owner_id", :dependent => :destroy
   has_and_belongs_to_many :rooms, :class_name => "Room", :foreign_key => "member_id", :association_foreign_key => "room_id"
 
   validates :name, presence: true, length: { minimum: 6 }, uniqueness: { case_sensitive: false }
